e31dd8f70a0e70928d54cd8fd9be487e44f412b1
Pobarul-islam <pobarul3@gmail.com>
2022-03-27 20:01:44 +0600

    some code changed

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
       <nav className=" navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
           <a className="navbar-brand" href="#">
-            Online Store
+           Digital Device.com
           </a>
           <button
             className="navbar-toggler"
